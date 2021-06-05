b7efde0a899081b839c582006ed5bd1351ba5b2e
thanveer <thanveerul.haq@nielsen.com>
2021-06-05 19:53:21 +0530

    first commit

---
+++ src/main/java/com/amz/clone/AmzCloneApplication.java
@@ -0,0 +1,13 @@
+package com.amz.clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmzCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmzCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amz/clone/AmzCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amz.clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmzCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
